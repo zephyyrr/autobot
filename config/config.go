0666a1a6f498d71f5d68625507007a19d92e0001
Johan Fogelström <zapmew@gmail.com>
2014-09-30 17:06:18 +0200

    made basic config definition

--- config/config.go
+++ config/config.go
@@ -7,6 +7,39 @@ import (
 )
 
 type Config struct {
+	Address string
+	Port    int
+
+	Events map[string][]Action
+}
+
+const (
+	Deployment = "deployment"
+	Push       = "push"
+	Ping       = "ping"
+	Release    = "release"
+)
+
+type Type string
+
+const (
+	Command Type = "command" // Calls <Command>
+	Install      = "install" // Calls go install <Package>
+	Test         = "test"    //Calls go test <Package>
+)
+
+type Action struct {
+	Type Type
+	//Filter Filter
+
+	Payload string
+}
+
+type Filter struct {
+	Owner      string
+	Repository string
+	//Branch      string // Only if the event regards this branch. Not sure how to implement at this time
+	Environment string
 }
 
 func LoadConfig(r io.Reader) (c Config, err error) {
