93409c6494e4c4c986b2a99f9c12fdff2d2317b3
Johan Fogelström <zapmew@gmail.com>
2014-09-30 14:07:03 +0200

    Overhaul of routes. Simplifies configuration github side.

--- webservr.go
+++ webservr.go
@@ -1,26 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func init() {
-	http.HandleFunc("/push", handlePush)
-	http.HandleFunc("/deploy", handleDeploy)
-	http.HandleFunc("/release", handleRelease)
+	http.HandleFunc("/hook", handleHook)
+	http.HandleFunc("/", handleRoot)
 }
 
-func handlePush(w http.ResponseWriter, req *http.Request) {
-	processes.Add(1)
-	defer processes.Done()
-	http.Error(w, "", http.StatusNotImplemented)
+func handleRoot(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(w, "<html><body>")
+	fmt.Fprintln(w, "Welcome to Autobot!<br>")
+	fmt.Fprintln(w, "Autobot is designed to be interfaced with Github's Webhooks API.<br>")
+	fmt.Fprintf(w, "To use, setup your projects Github repository to use the Webhook %s.<br>\n", hookAddress(req))
+	fmt.Fprintln(w, "</body></html>")
 }
 
-func handleDeploy(w http.ResponseWriter, req *http.Request) {
-	processes.Add(1)
-	defer processes.Done()
-	http.Error(w, "", http.StatusNotImplemented)
-}
-
-func handleRelease(w http.ResponseWriter, req *http.Request) {
+func handleHook(w http.ResponseWriter, req *http.Request) {
 	processes.Add(1)
 	defer processes.Done()
 	http.Error(w, "", http.StatusNotImplemented)
