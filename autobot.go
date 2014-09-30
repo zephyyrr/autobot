a99eb2416e54963211dc2b5c2c0e65595ff87f4d
Johan Fogelström <zapmew@gmail.com>
2014-09-30 12:33:12 +0200

    Added loading of config file as part of workflow

--- autobot.go
+++ autobot.go
@@ -1,25 +1,56 @@
 package main
 
 import (
+	"flag"
+	"log"
 	"net"
 	"net/http"
 	"os"
 	"os/signal"
 	"sync"
+
+	"github.com/zephyyrr/autobot/config"
 )
 
-var processes sync.WaitGroup
+var (
+	configuration Config
+	processes     sync.WaitGroup
+	configfile    = flag.String("f", "Config filename", "autobot.json")
+)
+
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	flag.Parse()
+	f, err := os.Open(configfile)
+	if err != nil {
+		log.Fatalln("Unable to read config file.", f)
+	}
+	config.LoadConfig(f)
+}
 
 func main() {
 	interrupt := make(chan os.Signal)
 	signal.Notify(interrupt, os.Interrupt)
 
-	conn := net.Listen("tcp", ":19840")
+	port := "8080"
+	if p := os.Getenv("PORT"); p != "" {
+		port = p
+	}
 
-	http.Serve(conn, http.DefaultServeMux)
+	conn, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalln("Error listening to port.", err)
+	}
+
+	go http.Serve(conn, http.DefaultServeMux)
 
 	select {
 	case <-interrupt:
+	}
+
+	if err := conn.Close(); err != nil {
+
 	}
 
 	shutdown()
@@ -30,7 +61,7 @@ func rollOut() {
 }
 
 func shutdown() {
-	conn.Close()
+
 	// Allows clean shutdowns.
 	processes.Wait()
 }
